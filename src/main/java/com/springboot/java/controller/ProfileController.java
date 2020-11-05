724bc89905c98b0459a1806289343a097b24c4dd
yaroslav-tsuprak <yaroslav.tsuprak@gmail.com>
2020-11-05 19:24:52 +0700

    Removed trash

--- src/main/java/com/springboot/java/controller/ProfileController.java
+++ src/main/java/com/springboot/java/controller/ProfileController.java
@@ -1,13 +1,8 @@
 package com.springboot.java.controller;
 
-import com.springboot.java.entity.Mailbox;
 import com.springboot.java.entity.Profile;
-import com.springboot.java.entity.User;
 import com.springboot.java.repository.ProfileRepository;
-import com.springboot.java.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,9 +17,6 @@ public class ProfileController {
 
     @Autowired
     ProfileRepository profileRepository;
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/admin/list-profiles")
     public String showProfiles(Model model) {
